b43914ab86c2687464ae1f839339d69aaa3f84b1
Chris Chappell <jkidd0170@gmail.com>
2024-04-16 03:11:17 -0400

    First Commit

---
+++ Team.py
@@ -0,0 +1,23 @@
+import random
+#Team 1
+players = ["Martain", "Craig", "Sue", "Claire", "Dave", "Alice", "Luciana",
+            "Harry", "Jack", "Rose", "Lexi", "Maria", "Thomas",
+            "James", "William", "Ada", "Grace", "Jean", "Marissa", "Alan"]
+print("Welcome to Team Allocator")
+while True:
+ random.shuffle(players)
+ team1 = players[:len(players)//2]
+ print("Team 1 captain: " + random.choice(team1))
+ print("Team 1: ")
+ for player in team1:
+    print(player)
+
+ #Team 2
+ team2 = players[len(players)//2:]
+ print("\nTeam 2 captain: "+ random.choice(team2))
+ for player in team2:
+    print(player)
+
+ response = input("Pick teams again? Type y or n: ")
+ if response == "n" : 
+   break
